529ddb5418cdeb222c2624ae5afe3a6a78e4e809
andy-b23 <andy-b23@gmx.at>
2018-12-01 11:39:26 +0100

    calculator place

--- Calculator.py
+++ Calculator.py
@@ -3,20 +3,20 @@ import tkinter.messagebox as mb
 
 w = tk.Tk()
 w.title("Calculator")
-w.geometry("400x125")
+w.geometry("365x125")
 w.resizable(False,False)
 
-tk.Label(w, text ="Input Number 1:").place(row=0, column=0,  padx=(5,5), pady=(5,5))
+tk.Label(w, text ="Input Number 1:").place(width= 125, height=20, x=5, y=5)
 e1 = tk.Entry(w)
-e1.grid(row=0, column=1, columnspan = 4)
+e1.place(width=235, height=20 , x=125, y=5 )
 
-tk.Label(w, text ="Input Number 2:").grid(row=1, column=0, padx=(5,5), pady=(5,5) )
+tk.Label(w, text ="Input Number 2:").place(width= 125, height=20, x=5, y=25)
 e2 = tk.Entry(w)
-e2.grid(row=1, column=1, columnspan = 4)
+e2.place(width=235, height=20 , x=125, y=25 )
 
-tk.Label(w, text ="Result:").grid(row=3, column=0, padx=(5,5), pady=(5,5))
+tk.Label(w, text ="Result:").place(width= 70, height=20, x= 5, y= 85)
 res = tk.Entry(w)
-res.grid(row=3, column=1, columnspan = 4)
+res.place(width= 235, height=20, x=125, y =85, )
 
 def plus():
     try:
@@ -26,7 +26,7 @@ def plus():
         res.insert(0,numb1+numb2)
     except ValueError:
         mb.showerror("Error", "Nur Zahlen eingeben. Punkt für Komma")
-    return
+
 def minus():
     try:
         numb1 = float(e1.get())
@@ -35,7 +35,7 @@ def minus():
         res.insert(0,numb1-numb2)
     except ValueError:
         mb.showerror("Error", "Nur Zahlen eingeben. Punkt für Komma")
-    return
+
 def mal():
     try:
         numb1 = float(e1.get())
@@ -44,21 +44,24 @@ def mal():
         res.insert(0,numb1*numb2)
     except ValueError:
         mb.showerror("Error", "Nur Zahlen eingeben. Punkt für Komma")
-    return
+
 def div():
     try:
         numb1 = float(e1.get())
         numb2 = float(e2.get())
         res.delete(0, "end")
-        res.insert(0,numb1/numb2)
-    except ValueError:
-        mb.showerror("Error", "Nur Zahlen eingeben. Punkt für Komma")
-    return
+        res.insert(0, numb1 / numb2)
+    except (ValueError, ZeroDivisionError) as e:
+        if isinstance(e, ValueError):
+            mb.showerror("Error", "Nur Zahlen eingeben. Punkt für Komma")
+        else:
+            mb.showerror("Error", "Division durch 0")
 
-b_plus = tk.Button(w, text="+", command=plus, width=5).grid(row=2,column=1)
-b_minus = tk.Button(w, text="-", command=minus, width=5).grid(row=2,column=2)
-b_mal = tk.Button(w, text="*", command=mal, width=5).grid(row=2,column=3)
-b_div = tk.Button(w, text="/", command=div, width=5).grid(row=2,column=4)
+
+b_plus = tk.Button(w, text="+", command=plus).place(width= 90, height=30 , x=5, y=50)
+b_minus = tk.Button(w, text="-", command=minus).place(width= 90, height=30, x=95, y=50)
+b_mal = tk.Button(w, text="*", command=mal).place(width= 90, height=30, x=185, y=50)
+b_div = tk.Button(w, text="/", command=div).place(width= 90, height=30, x=275, y=50)
 
 
 w.mainloop()
